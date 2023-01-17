c4f72b67901d69903f66f59bc2ccb9e815137b7e
Fraser Chapman <Fraser.Chapman@autotrader.co.uk>
2023-01-17 08:52:45 +0000

    rename classes and variables/constants to be more generic

--- src/main/java/com/example/cachebugdemo/configuration/MyCache.java
+++ src/main/java/com/example/cachebugdemo/configuration/MyCache.java
@@ -11,15 +11,15 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
-public class KillSwitchCache {
-    public static final String KILL_SWITCH_CACHE_MANAGER = "kill-switch-cache-manager";
+public class MyCache {
+    public static final String MY_CACHE_MANAGER = "my-cache-manager";
 
     private static final int MAXIMUM_CACHE_SIZE = 1000;
     private static final int TIME_TO_LIVE = 60;
 
-    @Bean(name = KILL_SWITCH_CACHE_MANAGER)
-    public CacheManager killSwitchCache() {
-        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("killSwitch");
+    @Bean(name = MY_CACHE_MANAGER)
+    public CacheManager myCache() {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("myCache");
 
         caffeineCacheManager.setCaffeine(Caffeine.newBuilder()
                 .maximumSize(MAXIMUM_CACHE_SIZE)
